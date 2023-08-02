bf4354904dfc1109c057f1721649aa9aee2eb331
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-02 17:03:05 +0530

    app update

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { AppRegistry } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 
 import { CustomDrawer } from './navigation/CustomDrawer'
 
-import {configStore, applyMiddleware} from 'redux';
+import {configureStore, applyMiddleware, } from '@reduxjs/toolkit';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import rootReducer from './stores/rootReducer.js';
 const Stack = createStackNavigator();
 
-const store = configStore(
-  rootReducer,
-  applyMiddleware(thunk)
-)
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: [...applyMiddleware(thunk)],
+  });
 
 const App = () => {
     return (
